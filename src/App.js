298d27df39d15561ff1dc5271040084beb303253
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-27 21:59:29 +0200

    defined app container dimensions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+import "./App.css"
+
 function App() {
   return (
-    <div>
-      
+    <div className="app-container">
+      <Header />
     </div>
   );
 }
