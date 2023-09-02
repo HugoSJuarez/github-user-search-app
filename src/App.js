ab6ec5393c987c151a9e35ed73675ad6f690b752
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-09-01 18:28:38 -0600

    tested Github API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Octokit } from "octokit";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar"
 import "./App.css"
 
-function App() {
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_AUTH_KEY
+});
+
+const user = await octokit.request('GET /users/{username}', {
+  username: 'octocat',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+});
+
+function App() {  
   return (
     <div className="app-container">
       <Header />
